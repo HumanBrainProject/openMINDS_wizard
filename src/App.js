58cb75863c2e35f5597a49de90d5902667d8d6e9
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2021-06-28 14:37:14 +0200

    initial commit
    Co-authored-by: Gilles Dénervaud <gilles.denervaud@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,146 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from '@rjsf/bootstrap-4';
 
-function App() {
+const schema = {
+  "title": "Dataset form",
+  "type": "object",
+  "properties": {
+    "name": {
+      "type": "string",
+      "title": "Name"
+    },
+    "description": {
+      "type": "string",
+      "title": "Description"
+    },
+    "doi": {
+      "type": "string",
+      "title": "DOI"
+    },
+    "homepage" : {
+      "type": "string",
+      "title": "Homepage"
+    },
+    "versionNumber" : {
+      "type": "string",
+      "title": "Version number"
+    },
+    "authors" : {
+      "type": "array",
+      "title": "Authors",
+      "items": {
+        "type": "string" 
+      }
+    },
+    "custodian" : {
+      "type": "array",
+      "title": "Custodian",
+      "items": {
+        "type": "string" 
+      }
+    },
+    "otherContributions" : {
+      "type": "string",
+      "title": "Other contributions"
+    },
+    "accessibility": {
+      "type": "string",
+      "title": "Accessibility",
+      "examples": [
+        "Free access",
+        "Embargo"
+      ]
+    },
+    "repositoryUrl" : {
+      "type": "string",
+      "title": "Repository URL"
+    },
+    "type": {
+      "type": "string",
+      "title": "Type",
+      "examples": [
+        "type1",
+        "type2"
+      ]
+    },
+    "license": {
+      "type": "string",
+      "title": "License",
+      "examples": [
+        "apache",
+        "mit"
+      ]
+    },
+    "fullDocumentation" : {
+      "type": "string",
+      "title": "Full documentation"
+    },
+    "keyword" : {
+      "type": "array",
+      "title": "Keyword",
+      "items": {
+        "type": "string" 
+      }
+    },
+    "copyrightHolderAndYear" : {
+      "type": "string",
+      "title": "Copyright holder & year"
+    },
+    "releasedDate":{
+      "type": "string",
+      "format": "date",
+      "title": "Released date"
+    },
+    "ethicsAssessment": {
+      "type": "string",
+      "title": "Ethics assessment",
+      "examples": [
+        "type1",
+        "type2"
+      ]
+    },
+    "experimentalApproach": {
+      "type": "string",
+      "title": "Experimental approach",
+      "examples": [
+        "type1",
+        "type2"
+      ]
+    },
+    "funding" : {
+      "type": "string",
+      "title": "Funding"
+    },
+    "relatedPublication": {
+      "type": "array",
+      "title": "Related publication",
+      "items": {
+        "type": "string" 
+      }
+    },
+    "supportChannel": {
+      "type": "array",
+      "title": "Support channel",
+      "items": {
+        "type": "string" 
+      }
+    },
+    "inputData" : {
+      "type": "string",
+      "title": "Input data"
+    }
+  }
+};
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="openMINDS_logo.png" alt="openminds" height="100" />
       </header>
+      <div className="form">
+        <Form schema={schema} />
+      </div>
     </div>
   );
 }
